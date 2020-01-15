f8561cbe8849d9b00ab5d1b4aad49d0fe6d55661
Briana Nzivu <bjnzivu@gmail.com>
2020-01-16 01:18:56 +0300

    st splash and view pager

---
+++ app/src/main/java/com/example/vybe/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.vybe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT =1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intent =new Intent(MainActivity.this,ViewPager.class);
+                startActivity(intent);
+                finish();
+            }
+
+        },SPLASH_TIME_OUT);
+    }
+}
