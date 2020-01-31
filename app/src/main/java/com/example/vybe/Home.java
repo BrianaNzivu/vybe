3032c39b06cbae6ee81801f68aafdbf4901f80fa
Briana Nzivu <bjnzivu@gmail.com>
2020-02-01 01:48:41 +0300

    st starting on Vision Board

--- app/src/main/java/com/example/vybe/Home.java
+++ app/src/main/java/com/example/vybe/Home.java
@@ -2,13 +2,27 @@ package com.example.vybe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
+    private ImageView myVision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        myVision=(ImageView) findViewById(R.id.hVison);
+
+        myVision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(Home.this ,VisionBoard.class);
+                startActivity(intent);
+            }
+        });
     }
 }
