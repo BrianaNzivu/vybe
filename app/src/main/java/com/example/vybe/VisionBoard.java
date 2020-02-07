1eedd588e39cd38b5d2623eb0d9841a30dad4cea
Briana Nzivu <bjnzivu@gmail.com>
2020-02-08 02:58:02 +0300

    st Vision Board layouts

--- app/src/main/java/com/example/vybe/VisionBoard.java
+++ app/src/main/java/com/example/vybe/VisionBoard.java
@@ -2,13 +2,30 @@ package com.example.vybe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class VisionBoard extends AppCompatActivity {
+    private Button cVision;
+    private ImageView vback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vision_board);
+
+        cVision =(Button) findViewById (R.id.bVisionOne);
+        vback=(ImageView) findViewById(R.id.hVison);
+
+        cVision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(VisionBoard.this ,VisionBoardEnt.class);
+                startActivity(intent);
+            }
+        });
     }
 }
