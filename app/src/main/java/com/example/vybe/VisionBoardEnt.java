316cf92ef08d4b16a3e6f79540c32dd3ffe65d76
Briana Nzivu <bjnzivu@gmail.com>
2020-02-08 03:06:10 +0300

    ft beginning VisionBoard back-end

--- app/src/main/java/com/example/vybe/VisionBoardEnt.java
+++ app/src/main/java/com/example/vybe/VisionBoardEnt.java
@@ -10,5 +10,6 @@ public class VisionBoardEnt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vision_board_ent);
+
     }
 }
