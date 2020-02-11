2e0f9009c0d4fca61b329f411ff54c1fb772c714
Briana Nzivu <bjnzivu@gmail.com>
2020-02-11 18:41:26 +0300

    st Date Picker

--- app/src/main/java/com/example/vybe/VisionBoardEnt.java
+++ app/src/main/java/com/example/vybe/VisionBoardEnt.java
@@ -1,15 +1,88 @@
 package com.example.vybe;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
 
 public class VisionBoardEnt extends AppCompatActivity {
+    private static TextView DateEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vision_board_ent);
+        DateEdit = (TextView) findViewById(R.id.vDate);
+        DateEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTruitonDatePickerDialog(v);
+            }
+        });
+    }
+
+    public void showTruitonDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+            DateEdit.setText(day + "/" + (month + 1) + "/" + year);
+        }
+    }
+//
+//    public void showTruitonTimePickerDialog(View v) {
+//        DialogFragment newFragment = new TimePickerFragment();
+//        newFragment.show(getSupportFragmentManager(), "timePicker");
+//    }
+//
+//    public static class TimePickerFragment extends DialogFragment implements
+//            TimePickerDialog.OnTimeSetListener {
+//
+//        @Override
+//        public Dialog onCreateDialog(Bundle savedInstanceState) {
+//            // Use the current time as the default values for the picker
+//            final Calendar c = Calendar.getInstance();
+//            int hour = c.get(Calendar.HOUR_OF_DAY);
+//            int minute = c.get(Calendar.MINUTE);
+//
+//            // Create a new instance of TimePickerDialog and return it
+//            return new TimePickerDialog(getActivity(), this, hour, minute,
+//                    DateFormat.is24HourFormat(getActivity()));
+//        }
+//
+//        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//            // Do something with the time chosen by the user
+//            DateEdit.setText(DateEdit.getText() + " -" + hourOfDay + ":" + minute);
+//        }
+
 
     }
-}
